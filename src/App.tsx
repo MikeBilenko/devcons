d37023388566069a75257302347e076992a42d1e
mike <mike@cdontiy.com>
2024-05-18 14:50:45 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <img src="/logo/logo.svg" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
